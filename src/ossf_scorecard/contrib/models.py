32a4255908cd96279efd6d2f4cbc567a094bf32f
404-geek <pranayd61@gmail.com>
2024-07-06 01:33:22 +0530

    score checks data python models  nexB#3
    Signed-off-by: 404-geek <pranayd61@gmail.com>

--- src/ossf_scorecard/contrib/models.py
+++ src/ossf_scorecard/contrib/models.py
@@ -1,5 +1,9 @@
 import attr
 
+from commoncode.datautils import Date
+from commoncode.datautils import String
+from commoncode.datautils import List
+
 class ModelMixin:
     """
     Base mixin for all package models.
@@ -27,6 +31,25 @@ class ModelMixin:
         known_attr = attr.fields_dict(cls)
         kwargs = {k: v for k, v in mapping.items() if k in known_attr}
         return cls(**kwargs)
+
+def to_tuple(collection):
+    """
+    Return a tuple of basic Python values by recursively converting a mapping
+    and all its sub-mappings.
+    For example::
+    >>> to_tuple({7: [1,2,3], 9: {1: [2,6,8]}})
+    ((7, (1, 2, 3)), (9, ((1, (2, 6, 8)),)))
+    """
+    if isinstance(collection, dict):
+        collection = tuple(collection.items())
+    assert isinstance(collection, (tuple, list))
+    results = []
+    for item in collection:
+        if isinstance(item, (list, tuple, dict)):
+            results.append(to_tuple(item))
+        else:
+            results.append(item)
+    return tuple(results)
 
 
 @attr.attributes(slots=True)
@@ -61,3 +84,65 @@ class PackageScoreMixin(ModelMixin):
         repr=True,
         label='score date',
         help='score date')
+
+
+    @classmethod
+    def from_data(cls, scorecard_data):
+        """
+        Return PackageData object created out of the package metadata
+        present in `scorecard_data` mapping. Also populate license and
+        copyright holder fields by computing them from extracted license
+        statement and extracted copyright.
+
+        Skip the license/copyright detection step if `package_only` is True.
+        """
+        if "purl" in scorecard_data:
+            scorecard_data.pop("purl")
+
+        scorecard_data = cls(**scorecard_data)
+
+        if not package_only:
+            scorecard_data.populate_license_fields()
+            scorecard_data.populate_holder_field()
+        else:
+            scorecard_data.normalize_extracted_license_statement()
+
+        return scorecard_data
+
+
+@attr.attributes(slots=True)
+class ScorecardChecksMixin(ModelMixin):
+
+    for_package = List(
+        item_type=PackageScoreMixin,
+        label = 'scoring tool',
+        help = 'Defines the source of a score or any other scoring metrics'
+           'For example: ossf-scorecard for scorecard data'
+    )
+
+    check_name = String(
+        repr=True,
+        label='scoring tool',
+        help='Defines the source of a score or any other scoring metrics'
+             'For example: ossf-scorecard for scorecard data')
+
+    check_score = String(
+        repr=True,
+        label='scoring tool version',
+        help='Defines the version of the scoring tool used for scanning the package')
+
+    reason = String(
+        repr=True,
+        label='score',
+        help='Score of the package which is scanned')
+
+    details = String(
+        repr=True,
+        label='scoring documentation url',
+        help='Version of the package as a string.')
+
+    score_date = Date(
+        repr=True,
+        label='score date',
+        help='score date')
+
